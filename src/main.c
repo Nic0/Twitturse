4a79901cd201d434706ab423239367a7defd66e5
Nic0 <nicolas.caen@gmail.com>
2010-05-02 13:28:39 +0200

    a good start... 	modified:   main.c 	utils.c 	utils.h

--- src/main.c
+++ src/main.c
@@ -3,8 +3,64 @@
 #include <string.h>
 #include <errno.h>
 
+#include <curl/curl.h>
+
+#include "utils.h"
+
+#define ERROR fprintf (stderr, \
+        "%s:%d Error (%d) : %s\n", \
+        __FILE__, __LINE__, \
+        errno, strerror(errno))
+
+char *
+get_URL (const char *url)
+{
+    CURL *curl = NULL;
+    CURLcode result;
+    string_t *str;
+    char *res;
+
+    if (NULL == (str = string_new ())) {
+        ERROR;
+        return NULL;
+    }
+
+    curl = curl_easy_init ();
+
+    if (curl != NULL) {
+        curl_easy_setopt (curl, CURLOPT_URL, url);
+        curl_easy_setopt (curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_easy_setopt (curl, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_easy_setopt (curl, CURLOPT_USERNAME, "nic0sphere");
+        curl_easy_setopt (curl, CURLOPT_PASSWORD, "6akw92c");
+        curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, str_append);
+        curl_easy_setopt (curl, CURLOPT_WRITEDATA, str);
+
+        result = curl_easy_perform (curl);
+        curl_easy_cleanup (curl);
+
+        if (CURLE_OK == result && (str->str) != NULL) {
+            res = str->str;
+            free(str);
+            return res;
+        } else {
+            ERROR;
+            return NULL;
+        }
+    } else {
+        free (str);
+        ERROR;
+        return NULL;
+    }
+}
+
 int main (void)
 {
-    printf("Hello World\n");
+    char *data;
+    data = get_URL ("https://api.twitter.com/statuses/home_timeline.xml");
+
+    if (data != NULL) {
+        printf ("%s", data);
+    }
     return EXIT_SUCCESS;
 }
