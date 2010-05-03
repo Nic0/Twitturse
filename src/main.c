69b2b71c4de6e466ecf92f5ed70faeb7f113c8bc
Nic0 <nicolas.caen@gmail.com>
2010-05-03 22:14:19 +0200

    xml.c|h removed… fiew prob with it 	modified:   main.c 	deleted:    xml.c 	deleted:    xml.h

--- src/main.c
+++ src/main.c
@@ -3,11 +3,9 @@
 #include <string.h>
 #include <errno.h>
 
-#include <curl/curl.h>
 #include <libxml/tree.h>
 #include <libxml/xpath.h>
 
-#include "utils.h"
 #include "curl.h"
 
 #define ERROR fprintf (stderr, \
@@ -32,6 +30,25 @@ initStatus (status_t *status)
     status->prev = (maloc sizeof(status_t));
 }*/
 
+static void print_element_names (xmlNode *a_node)
+{
+    xmlNode *cur_node = NULL;
+
+    for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
+    
+        if (cur_node->parent != NULL) { 
+            if (cur_node->type == XML_TEXT_NODE 
+                 && strcmp(cur_node->parent->name, "screen_name") == 0)
+                printf ("<%s>\t", cur_node->content);
+
+            if (cur_node->type == XML_TEXT_NODE 
+                 && strcmp(cur_node->parent->name, "text") == 0)
+                printf ("%s\n", cur_node->content);
+        }
+        print_element_names(cur_node->children);
+    }
+}
+
 int main (void)
 {
     char *data;
@@ -55,6 +72,6 @@ int main (void)
     print_element_names(xmlroot);
     
     xmlFreeDoc (xmldoc);
-    xmlCleanupParser();    
+    xmlCleanupParser();
     return EXIT_SUCCESS;
 }
