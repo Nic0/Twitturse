4eb3b60d7ca3fc6c074a5ca2aa17b2d4ad647166
Nic0 <nicolas.caen@gmail.com>
2010-05-03 23:56:24 +0200

    merge for the master, stable. 	modified:   main.c

--- src/main.c
+++ src/main.c
@@ -15,22 +15,44 @@
 
 typedef struct status_t
 {
-    xmlChar *pseudo;
-    xmlChar *text;
-    struct   status_t *next;
-    struct   status_t *prev;
+    unsigned long    id;
+    xmlChar         *pseudo;
+    xmlChar         *text;
+    struct status_t *next;
+    struct status_t *prev;
 } status_t;
+
+typedef struct statuses_t
+{
+    status_t    *first;
+    status_t    *last;
+    int          count;
+} statuses_t;
 
 status_t * 
 initStatus (status_t *status)
 {
+    status->id = 0;
     status->pseudo = NULL;
     status->text = NULL;
-    status->next = malloc (sizeof(status_t));
-    status->prev = malloc (sizeof(status_t));
+    if ((status->next = malloc (sizeof(status_t))) != NULL)
+        if ((status->prev = malloc (sizeof(status_t))) == NULL)
+            return NULL;
+    
+    return status;
 }
 
-/*static void print_element_names (xmlNode *a_node)
+statuses_t *
+initStatuses (statuses_t *statuses)
+{
+    if ((statuses->first = malloc (sizeof(status_t))) != NULL)
+        if ((statuses->last = malloc (sizeof(status_t))) == NULL)
+            return NULL;
+    return statuses;
+}
+
+static void 
+getStatuses (xmlNode *a_node)
 {
     xmlNode *cur_node = NULL;
 
@@ -45,36 +67,18 @@ initStatus (status_t *status)
                  && strcmp(cur_node->parent->name, "text") == 0)
                 printf ("%s\n", cur_node->content);
         }
-        print_element_names(cur_node->children);
-    }
-}*/
-
-static void print_element_names (xmlNode *a_node)
-{
-    xmlNode *cur_node = NULL;
-
-    for (cur_node = a_node; cur_node; cur_node = cur_node->children) {
-    
-        if (cur_node->parent != NULL) { 
-            if (cur_node->type == XML_TEXT_NODE 
-                 && strcmp(cur_node->parent->name, "screen_name") == 0)
-                printf ("<%s>\t", cur_node->content);
-
-            if (cur_node->type == XML_TEXT_NODE 
-                 && strcmp(cur_node->parent->name, "text") == 0)
-                printf ("%s\n", cur_node->content);
-        }
-        print_element_names(cur_node->next);
+        getStatuses(cur_node->children);
     }
 }
 
-int main (void)
+int 
+main (void)
 {
-    char *data;
+    char *data = NULL;
     data = get_URL ("https://api.twitter.com/statuses/home_timeline.xml");
     //data = get_URL ("http://search.twitter.com/search.atom?q=%23Archlinux");
 
-    xmlDocPtr xmldoc;
+    xmlDoc *xmldoc = NULL;
     xmlNode *xmlroot = NULL;
 
     if ((xmldoc = xmlParseMemory (data, strlen(data))) == NULL) {
@@ -88,7 +92,7 @@ int main (void)
         return EXIT_FAILURE;
     }
 
-    print_element_names(xmlroot);
+    getStatuses(xmlroot);
     
     xmlFreeDoc (xmldoc);
     xmlCleanupParser();
