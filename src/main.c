65c541b24420ba9298582c381c284a2b3ac196a5
Nic0 <nicolas.caen@gmail.com>
2010-05-04 11:29:49 +0200

    got a chain working, with last print last event 	modified:   main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,14 @@
+/***********************************************************
+*
+*       Twitturse   v 0.0.1
+*
+*       Nic0 <nicolas.caen (at) gmail.com>
+*       03/05/2010
+*
+*       Software distributed under GPL licence 
+*
+***********************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -32,54 +43,88 @@ typedef struct statuses_t
 status_t * 
 initStatus (status_t *status)
 {
-    status->id = 0;
-    status->pseudo = NULL;
-    status->text = NULL;
-    if ((status->next = malloc (sizeof(status_t))) != NULL)
-        if ((status->prev = malloc (sizeof(status_t))) == NULL)
-            return NULL;
-    
+    if ((status = malloc (sizeof(status_t))) == NULL)
+        return NULL;
+    status->id      = 0;
+    status->pseudo  = NULL;
+    status->text    = NULL;
+    status->next    = NULL;
+    status->prev    = NULL;
     return status;
 }
 
 statuses_t *
 initStatuses (statuses_t *statuses)
 {
-    if ((statuses->first = malloc (sizeof(status_t))) != NULL)
-        if ((statuses->last = malloc (sizeof(status_t))) == NULL)
-            return NULL;
+    if ((statuses = malloc (sizeof(statuses_t))) == NULL)
+        return NULL;
+    statuses->count = 0;
+    statuses->first = NULL;
+    statuses->last  = NULL;
     return statuses;
 }
 
 static void 
-getStatuses (xmlNode *a_node)
+getStatuses (xmlNode *a_node, statuses_t *statuses, status_t *cur_status)
 {
-    xmlNode *cur_node = NULL;
+    xmlNode     *cur_node   = NULL;
 
-    for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
-    
-        if (cur_node->parent != NULL) { 
-            if (cur_node->type == XML_TEXT_NODE 
-                 && strcmp(cur_node->parent->name, "screen_name") == 0)
-                printf ("<%s>\t", cur_node->content);
+    for (cur_node = a_node; cur_node; cur_node = cur_node->children) {
 
             if (cur_node->type == XML_TEXT_NODE 
-                 && strcmp(cur_node->parent->name, "text") == 0)
-                printf ("%s\n", cur_node->content);
-        }
-        getStatuses(cur_node->children);
+                 && strcmp(cur_node->parent->name, "screen_name") == 0) {
+                    cur_status->pseudo = cur_node->content;
+                    //printf ("<%s>\t", cur_node->content);
+            }
+
+            if (cur_node->type == XML_TEXT_NODE 
+                 && strcmp(cur_node->parent->name, "text") == 0) {
+
+                    cur_status->text = cur_node->content;
+
+                    status_t *node_status;
+                    if ((node_status = initStatus(node_status)) == NULL)
+                        ERROR;
+                    *node_status = *cur_status;
+
+                    if(statuses->count == 0) {
+                        statuses->last = node_status;
+                        statuses->first = node_status;
+                    } else {
+                        statuses->last->next = node_status;
+                        statuses->last = node_status;
+                    }
+
+                    cur_status = initStatus(cur_status);
+                    statuses->count++;   
+
+                    //printf ("%s\n", cur_node->content);
+            }
+
+        getStatuses(cur_node->next, statuses, cur_status);
+    }
+}
+
+void
+printStatuses (statuses_t *statuses)
+{
+    status_t *status = NULL;
+    status = statuses->first;
+    while (status->next != NULL) {
+        printf("<%s>\t%s\n", status->pseudo, status->text);
+        status = status->next;
     }
 }
 
 int 
 main (void)
 {
-    char *data = NULL;
-    data = get_URL ("https://api.twitter.com/statuses/home_timeline.xml");
-    //data = get_URL ("http://search.twitter.com/search.atom?q=%23Archlinux");
+    static char *data       = NULL;
+    xmlDoc      *xmldoc     = NULL;
+    xmlNode     *xmlroot    = NULL;
+    statuses_t  *statuses   = NULL;
 
-    xmlDoc *xmldoc = NULL;
-    xmlNode *xmlroot = NULL;
+    data = get_URL ("https://api.twitter.com/statuses/home_timeline.xml");
 
     if ((xmldoc = xmlParseMemory (data, strlen(data))) == NULL) {
         ERROR;
@@ -92,7 +137,16 @@ main (void)
         return EXIT_FAILURE;
     }
 
-    getStatuses(xmlroot);
+    if ((statuses = initStatuses(statuses)) == NULL) {
+        ERROR;
+        return EXIT_FAILURE;
+    }
+    
+    status_t *cur_status = NULL;
+    cur_status = initStatus(cur_status);
+    getStatuses(xmlroot, statuses, cur_status);
+
+    printStatuses(statuses);
     
     xmlFreeDoc (xmldoc);
     xmlCleanupParser();
